809ed0d6bbdcf17693ef8f983a04d47c7c298acb
Jose Quispe <joseft.jack18@gmail.com>
2024-03-16 15:06:31 -0500

    dfgdfg

--- database/migrations/2024_02_15_151941_create_person_history_table.php
+++ database/migrations/2024_02_15_151941_create_person_history_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('person_history', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('person_id');
-            $table->text('personal_history');
-            $table->text('allergies_history');
-            $table->text('family_history');
+            $table->text('personal_history')->nullable();
+            $table->text('allergies_history')->nullable();
+            $table->text('family_history')->nullable();
             $table->timestamps();
 
             $table->foreign('person_id')->references('id')->on('persons');
